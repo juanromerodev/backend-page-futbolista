50909472c9b38b524c99d7cccb2a9719e17f3eee
Juan Romero Dev <juancarlosromerodev@gmail.com>
2024-04-13 12:48:56 -0500

    Realizado Basic Authentication

--- FutbolistasAPI/src/main/java/com/example/solucion/controller/FutbolistaController.java
+++ FutbolistasAPI/src/main/java/com/example/solucion/controller/FutbolistaController.java
@@ -3,6 +3,7 @@ package com.example.solucion.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.example.solucion.service.FutbolistaService;
 
 @RestController
 @RequestMapping("/futbolistas")
+@CrossOrigin(origins = "http://localhost:3000")
 public class FutbolistaController {
     @Autowired
     private FutbolistaService futbolistaService;
